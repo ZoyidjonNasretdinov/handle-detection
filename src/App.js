579e2f265b3183da35fb3ecb9d93c623636e5934
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-03 21:24:49 +0500

    add utils file and hand Draw func

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import './App.css'
-import React, {useCallback, useEffect, useRef} from 'react'
-import * as tf from '@tensorflow/tfjs'
-import * as handPose from '@tensorflow-models/handpose'
-import Webcam from 'react-webcam'
-
+import './App.css';
+import React, { useCallback, useEffect, useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam';
+import { drawHand } from './utils';
 
 const App = () => {
-  const webcamRef = useRef(null)
-  const canvasRef = useRef(null)
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-  const runHandDetection = useCallback(async () => {
-    const model = await handPose.load(
-      handPose.SupportedPackages.mediapipeFacemesh || {},
-      {
-        runtime: 'tfjs',
-      }
-    );
-
-    const detectionInterval = setInterval(() => {
-      detect(model);
-    }, 100);
-
-    return () => clearInterval(detectionInterval);
-  }, [detect]);
-
-  useEffect(() => {
-    const cleanup = runHandDetection ();
-    return () => cleanup.then((clear) => clear && clear());
-  }, [runHandDetection]);
-
-  
   const detect = useCallback(async (model) => {
     if (
       webcamRef.current &&
@@ -39,27 +18,46 @@ const App = () => {
       const videoWidth = video.videoWidth;
       const videoHeight = video.videoHeight;
 
+      // Set video and canvas dimensions
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      const faces = await model.estimateFaces({ input: video });
+      // Make hand predictions
+      const hands = await model.estimateHands(video);
 
+      // Draw results
       const ctx = canvasRef.current.getContext('2d');
       ctx.clearRect(0, 0, videoWidth, videoHeight);
-      drawMesh(faces, ctx);
+      drawHand(hands, ctx);
     }
   }, []);
 
+  const runHandDetection = useCallback(async () => {
+    const model = await handpose.load(); // Correct model loading
+
+    const detectionInterval = setInterval(() => {
+      detect(model);
+    }, 100);
+
+    return () => clearInterval(detectionInterval);
+  }, [detect]);
+
+  useEffect(() => {
+    const cleanup = runHandDetection();
+    return () => cleanup.then((clear) => clear && clear());
+  }, [runHandDetection]);
+
+ 
   return (
     <div className='App'>
       <header className='App-header'>
         <Webcam ref={webcamRef} className='webcam' />
-        <canvas ref={canvasRef} className='canvas'/>
+        <canvas ref={canvasRef} className='canvas' />
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
