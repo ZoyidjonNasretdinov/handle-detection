aba5ae473d975eec8b3dd0258dd23a6cd9f669e7
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-03 20:59:07 +0500

    Install project and libraries

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import React, {useEffect, useRef} from 'react'
+import * as tf from '@tensorflow/tfjs'
+import * as handPose from '@tensorflow-models/handpose'
+import Webcam from 'react-webcam'
+
+
+const App = () => {
+  return (
+    <div className='App'>App</div>
+  )
+}
+
+export default App
